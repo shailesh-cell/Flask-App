04825b172b5fef39611b8ebffae855eec3434755
sc1 <s.c1@mail.com>
2025-03-01 23:52:10 +0530

    acr output.tf

--- modules/acr/main.tf
+++ modules/acr/main.tf
@@ -2,12 +2,12 @@
 data "azurerm_container_registry" "existing" {
   name                = "${var.app_name}acr${var.environment}"
   resource_group_name      = var.resource_group_name
-  count               = 1
 }
 
 # Create ACR only if it doesn't exist
 resource "azurerm_container_registry" "acr" {
   name                = "${var.app_name}acr${var.environment}"
+  count               = length(try(data.azurerm_container_registry.existing[0].id, "")) > 0 ? 0 : 1
   resource_group_name      = var.resource_group_name
   location            = var.location
   sku                 = "Standard"
@@ -15,6 +15,6 @@ resource "azurerm_container_registry" "acr" {
 }
 
 locals {
-  acr_id   = coalesce(try(data.azurerm_container_registry.existing.id, null), try(azurerm_container_registry.acr["create"].id, null))
-  acr_name = coalesce(try(data.azurerm_container_registry.existing.name, null), try(azurerm_container_registry.acr["create"].name, null))
+  acr_id   = coalesce(try(data.azurerm_container_registry.existing[0].id, null), try(azurerm_container_registry.acr[0].id, null))
+  acr_name = coalesce(try(data.azurerm_container_registry.existing[0].name, null), try(azurerm_container_registry.acr[0].name, null))
 }
