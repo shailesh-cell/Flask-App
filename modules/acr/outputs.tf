c295ddea4e374e7d4e76151c42f9a2d61f8ceba6
sc1 <s.c1@mail.com>
2025-03-02 01:02:05 +0530

    acr main for exisiting resource

--- modules/acr/outputs.tf
+++ modules/acr/outputs.tf
@@ -1,12 +1,11 @@
 output "acr_id" {
-  value = local.acr_exists ? data.azurerm_container_registry.existing.id : azurerm_container_registry.acr[each.key].id
+  value = local.acr_exists ? data.azurerm_container_registry.existing.id : (length(local.create_acr) > 0 ? azurerm_container_registry.acr.values().*id[0] : null)
 }
 
 output "acr_name" {
-  value = local.acr_exists ? data.azurerm_container_registry.existing.name : azurerm_container_registry.acr[each.key].name
+  value = local.acr_exists ? data.azurerm_container_registry.existing.name : (length(local.create_acr) > 0 ? azurerm_container_registry.acr.values().*name[0] : null)
 }
 
 output "login_server" {
-  value = local.acr_exists ? data.azurerm_container_registry.existing.login_server : azurerm_container_registry.acr[each.key].login_server
+  value = local.acr_exists ? data.azurerm_container_registry.existing.login_server : (length(local.create_acr) > 0 ? azurerm_container_registry.acr.values().*login_server[0] : null)
 }
-
