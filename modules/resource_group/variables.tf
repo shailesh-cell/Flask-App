3e4ad203ac2778d8e4609fa968d8f7ba48ad075f
sc1 <s.c1@mail.com>
2025-03-01 19:29:36 +0530

    resource group modified value

--- modules/resource_group/variables.tf
+++ modules/resource_group/variables.tf
@@ -1,2 +1,14 @@
-variable "name" {}
-variable "location" {}
+variable "app_name" {
+  description = "Application name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
