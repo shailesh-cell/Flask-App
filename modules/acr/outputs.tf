b441011e0aa8ae4e965df189078d61e10feef03d
sc1 <s.c1@mail.com>
2025-03-01 21:48:35 +0530

    resource group

--- modules/acr/outputs.tf
+++ modules/acr/outputs.tf
@@ -12,3 +12,18 @@ output "acr_login_server" {
   description = "The login server of the Azure Container Registry"
   value       = "${local.acr_name}.azurecr.io"
 }
+
+output "id" {
+  description = "The ID of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.id
+}
+
+output "login_server" {
+  description = "The login server of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.login_server
+}
+
+output "name" {
+  description = "The name of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.name
+}
--- modules/key_vault/outputs.tf
+++ modules/key_vault/outputs.tf
@@ -1,3 +1,13 @@
 output "key_vault_id" {
   value = azurerm_key_vault.kv.id
 }
+
+output "id" {
+  description = "The ID of the Key Vault"
+  value       = azurerm_key_vault.kv.id
+}
+
+output "name" {
+  description = "The name of the Key Vault"
+  value       = azurerm_key_vault.kv.name
+}
