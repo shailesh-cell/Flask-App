ab573393e006acae520b0d3cb3cc2d78aac67486
sc1 <s.c1@mail.com>
2025-03-01 18:31:18 +0530

    removed tenant id

--- providers.tf
+++ providers.tf
@@ -2,5 +2,4 @@ provider "azurerm" {
   features {}
 
   subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
 }
--- variables.tf
+++ variables.tf
@@ -17,8 +17,3 @@ variable "subscription_id" {
   description = "Azure Subscription ID"
   type        = string
 }
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
