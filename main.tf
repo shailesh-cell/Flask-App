29403696db3672fc7eb5f1609adddc03e2b1b853
sc1 <s.c1@mail.com>
2025-03-03 21:01:27 +0530

    validate

--- main.tf
+++ main.tf
@@ -6,18 +6,18 @@ module "resource_group" {
 }
 
 module "key_vault" {
-  source                    = "./modules/key_vault"
-  app_name                  = var.app_name
-  environment               = var.environment
-  location                  = module.resource_group.rg_location
-  tenant_id                 = var.tenant_id
-  resource_group_name       = module.resource_group.rg_name
-  acr_password              = var.acr_password
-  acr_username              = var.acr_username
-  acr_identity_object_id    = module.acr.acr_identity_principal_id
-  application_object_id                 = var.spn_object_id
-  spn_object_id                 = var.spn_object_id
-  depends_on                = [module.resource_group]
+  source                 = "./modules/key_vault"
+  app_name               = var.app_name
+  environment            = var.environment
+  location               = module.resource_group.rg_location
+  tenant_id              = var.tenant_id
+  resource_group_name    = module.resource_group.rg_name
+  acr_password           = var.acr_password
+  acr_username           = var.acr_username
+  acr_identity_object_id = module.acr.acr_identity_principal_id
+  application_object_id  = var.spn_object_id
+  spn_object_id          = var.spn_object_id
+  depends_on             = [module.resource_group]
 }
 
 # Ensure Key Vault depends on ACR
