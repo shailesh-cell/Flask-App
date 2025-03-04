b186fb3babdbe4eb91e7167134acad71492de92a
sc1 <s.c1@mail.com>
2025-03-04 23:58:56 +0530

    added acr secret folder

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -30,6 +30,8 @@ resource "azurerm_key_vault_access_policy" "acr_policy" {
   object_id    = var.acr_identity_principal_id # Passed from ACR module
 
   secret_permissions = ["Get", "List", "Set", "Delete"]
+
+  depends_on = [var.acr_identity_principal_id] # Ensures ACR is created first
 }
 
 # Store ACR Username in Key Vault
