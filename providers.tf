ff45ee2f4b16b9983a8159f475987e88bae63481
sc1 <s.c1@mail.com>
2025-03-06 22:50:30 +0530

    seperate aci

--- providers.tf
+++ providers.tf
@@ -1,9 +1,10 @@
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 
   subscription_id = "a6435a3c-312e-41ed-878b-3118525f919a"
-  tenant_id       = "21150d7d-bb5a-486f-86af-73c82a8240bc" # Your Tenant ID
-  resource_group {
-      prevent_deletion_if_contains_resources = false
-}
+  tenant_id       = "21150d7d-bb5a-486f-86af-73c82a8240bc"
 }
